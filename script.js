a2ef909e9a66f0367cddfd1fd14d48e996bf73c3
michael_heckmann <michael.heckmann@qart.shop>
2021-02-12 12:48:54 +0100

    from Applescript

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    // https://api.baseql.com/airtable/graphql/appT3rWz3MSO0mXFY
+
+    // fetch('https://api.baseql.com/airtable/graphql/appT3rWz3MSO0mXFY', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json',
+    //     },
+    //     body: JSON.stringify({ query: `
+    //     query {
+    //       home(login:"michael") {
+    //         login
+    //       }
+    //     }` 
+    //   }),
+    //   })
+    //     .then(r => console.log(r.status));
+}
+
+
+
